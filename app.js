aef5651a34f2a3fbf5fd051fbd82804b7879d1c9
Aphasia18 <102366245+Aphasia18@users.noreply.github.com>
2022-11-24 20:46:55 +0400

    update

--- app.js
+++ app.js
@@ -1,17 +1,7 @@
 const navigation = document.querySelector('.navigation');
-const toggleBtn = document.querySelector('.navigation-mobile-toggle')
+const toggleBtn = document.querySelector('.navigation-mobile-toggle');
 
-
-
-toggleBtn.addEventListener("click", () =>{
-    const visible = navigation.getAttribute("visible")
-
-    if (visible === "false"){
-        navigation.setAttribute("visible", true)
-        toggleBtn.setAttribute("expanded",true)
-    } else if (visible === "true") {
-        navigation.setAttribute("visible", false)
-        toggleBtn.setAttribute("expanded",false)
-    }
-})
-
+toggleBtn.onclick = function () {
+    toggleBtn.classList.toggle("active")
+    navigation.classList.toggle("active")
+}
